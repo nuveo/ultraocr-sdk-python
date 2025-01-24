c624fa3c095d7acaf776ca24cfed88271f250948
Kleberson Canuto <klebersoncanuto@gmail.com>
2025-01-24 15:54:19 -0300

    Version 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="ultraocr-sdk-python",
     packages=["ultraocr"],
-    version="0.1.0",
+    version="1.0.0",
     description="UltraOCR Python SDK",
     author="Nuveo",
     install_requires=["requests"],
